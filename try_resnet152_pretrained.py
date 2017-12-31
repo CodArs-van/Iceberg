9630aab17a8c24442584c5986ccd9a4dfad6119c
Xiangyu Guo <csgxy123@hotmail.com>
2017-12-31 22:52:21 +0000

    resnet152 added

---
+++ try_resnet152_pretrained.py
@@ -0,0 +1,19 @@
+from models.resnet import resnet152
+from iceberg import *
+from torch.optim import SGD
+
+lr = 0.0005
+mom = 0.9
+wd = 1e-4
+bsize = 172
+
+model = resnet152(True)
+model = torch.nn.DataParallel(model).cuda()
+optim = SGD(model.parameters(), lr, momentum=mom, weight_decay=wd)
+iceberg = Iceberg('./data/train.json', 
+                  './data/resnet152_lr5e-4_mom9e-1_wd1e-4_bs{}_pretrained_model.pth'.format(bsize), model, optim, 10000, bsize)
+iceberg.run(transforms.Compose([
+            transforms.Resize(256),
+            transforms.RandomCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor()]))
