0a6f0c9ceaf04ae2b60458519c5d2798eddf8764
Xiangyu Guo <csgxy123@hotmail.com>
2018-01-01 00:01:04 +0000

    update backup.py

--- backup.py
+++ backup.py
@@ -1,7 +1,18 @@
 import utils
+import argparse
+
+parser = argparse.ArgumentParser(description='copy source dir to destination dir every given seconds')
+parser.add_argument('-s', '--src', default='', type=str, metavar='PATH', help='path to source dir (default: "")')
+parser.add_argument('-d', '--dst', default='', type=str, metavar='PATH', help='path to destination dir (default: "")')
+parser.add_argument('-t', '--time', default=600, type=int, metavar='N', help='interval seconds to backup')
 
 def ignore(src, dst):
     return ['train.json', 'train.json.7z', 'test.json', 'test.json.7z', 'stinkbug.png', 'sample_submission.csv', 'sample_submission.csv.7z']
 
 if __name__ == '__main__':
-    utils.backup('/home/iceberg/data', '/var/storage/shared/pnrsy/sys/jobs/application_1513627406021_6970/data/iceberg', False, ignore, 600)
+    args = parser.parse_args()
+    assert args.src, 'source dir is none'
+    assert args.dst, 'distination dir is none'
+    assert args.time > 0, 'interval should not be negative'
+    
+    utils.backup(args.src, args.dst, False, ignore, args.time)
