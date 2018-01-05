98ab22e81525eeba81b3860477ded85175716d92
Xiangyu Guo <csgxy123@hotmail.com>
2018-01-05 04:12:12 +0000

    resnet18 update

--- try_resnet18.py
+++ try_resnet18.py
@@ -2,15 +2,15 @@ from models.resnet import resnet18
 from iceberg import *
 from torch.optim import SGD
 
-lr = 0.0005
-mom = 0.9
-wd = 1e-4
-bsize = 128
+lr = '1e-3'
+mom = '95e-2'
+wd = '1e-4'
+bsize = 64
 
 model = resnet18(False, num_classes=2)
 model = torch.nn.DataParallel(model).cuda()
-optim = SGD(model.parameters(), lr, momentum=mom, weight_decay=wd)
-iceberg = Iceberg('./data/train.json', './data/resnet18_lr5e-4_mom9e-1_wd1e-4_bs{}_model.pth'.format(bsize), model, optim, 512, bsize)
+optim = SGD(model.parameters(), float(lr), momentum=float(mom), weight_decay=float(wd))
+iceberg = Iceberg('./data/train.json', './data/resnet18_lr{}_mom{}_wd{}_bs{}_model.pth'.format(lr, mom, wd, bsize), model, optim, 128, bsize)
 iceberg.run(transforms.Compose([
             transforms.Resize(256),
             transforms.RandomCrop(224),
